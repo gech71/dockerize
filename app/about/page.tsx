3eb495e6790689e14fef934fa52c8891b89cc7b9
Getaye Temesgen <tgech71@gmail.com>
2026-01-14 17:15:28 +0300

    chore: correct description text on About Page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default function AboutPage() {
             About Page
           </h1>
           <p className="text-zinc-600 dark:text-zinc-400">
-            This is a secondary page in your Next.js App Router demo.
+            This is about page in your Next.js App Router demo.
           </p>
         </div>
 
