3354514bcb80cccb9b15aa73f4a65257fd5514d6
Getaye Temesgen <tgech71@gmail.com>
2026-01-16 19:16:58 +0300

    chore: update Docker image reference in deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 # ==========================
 # CONFIG
 # ==========================
-$IMAGE = "nextjs-web:latest"
+$IMAGE = "ghcr.io/gech71/next-docker-app:latest"
 
 $OLD_CONTAINER = "nextjs-prod"
 $NEW_CONTAINER = "nextjs-prod-new"
